02a77fbf76fb2650fcfc004c258bb932198af4bd
Saba Vartasashvili <vartasashvili94@gmail.com>
2022-03-06 17:01:11 +0400

    user only can select dates between 2019-today

--- src/components/Covid/CovidStuff/InputFielss/LastVaccine.js
+++ src/components/Covid/CovidStuff/InputFielss/LastVaccine.js
@@ -4,6 +4,15 @@ import styles from "./LastVaccine.module.css";
 import Calendar from "../../../assets/calendar.svg";
 const LastVaccine = () => {
   const userData = useContext(Context);
+
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  today = yyyy + "-" + mm + "-" + dd;
+
   return (
     <>
       {userData.vaccinated && (
@@ -11,7 +20,13 @@ const LastVaccine = () => {
           <p className={styles["when__txt2"]}>
             When did you get your last covid vaccine?
           </p>
-          <input type="date" placeholder="Date" className={styles.date2} />
+          <input
+            type="date"
+            placeholder="Date"
+            className={styles.date2}
+            max={today}
+            min="2019-01-01"
+          />
           <img
             src={Calendar}
             alt="calendar icon"
--- src/components/Covid/CovidStuff/InputFielss/When.js
+++ src/components/Covid/CovidStuff/InputFielss/When.js
@@ -2,15 +2,33 @@ import styles from "./When.module.css";
 import Calendar from "../../../assets/calendar.svg";
 import { useContext } from "react";
 import Context from "../../../store/context";
+
 const When = () => {
   const userData = useContext(Context);
-
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1;
+  let yyyy = today.getFullYear();
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  today = yyyy + "-" + mm + "-" + dd;
+  const dateHandler = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <>
       {userData.hadCovid && (
         <>
           <p className={styles["when__txt"]}>When?</p>
-          <input type="date" placeholder="Date" className={styles.date} />
+          <input
+            id="datefield"
+            type="date"
+            placeholder="Date"
+            className={styles.date}
+            onChange={dateHandler}
+            max={today}
+            min="2019-01-01"
+          />
           <img src={Calendar} alt="calendar icon" className={styles.calendar} />
         </>
       )}
